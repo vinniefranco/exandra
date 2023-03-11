5616832b2a40a86c43565520740d5ee97b3f9549
Vinnie Frano <vince@freshivore.net>
2023-03-11 16:07:47 -0600

    Removing dead types

--- lib/exandra/types.ex
+++ lib/exandra/types.ex
@@ -1,8 +1,6 @@
 defmodule Exandra.Types do
-  alias Exandra.Types.XList
   alias Exandra.Types.XMap
   alias Exandra.Types.XSet
-  alias Exandra.Types.XJson
 
   def apply(type, op, value, opts) do
     cond do
@@ -22,19 +20,15 @@ defmodule Exandra.Types do
   def for(:integer), do: :int
   def for(:string), do: :text
   def for(:binary), do: :blob
-  def for(:map), do: XJson.type(:map)
 
   def for(t) when t in [:naive_datetime, :utc_datetime, :utc_datetime_usec],
     do: :timestamp
 
   def for({:parameterized, Ecto.Embedded, _}), do: :text
   def for({:parameterized, Ecto.Enum, _}), do: :text
-  def for({:parameterized, XList, opts}), do: XList.xandra_type(opts)
   def for({:parameterized, XMap, opts}), do: XMap.xandra_type(opts)
   def for({:parameterized, XSet, opts}), do: XSet.xandra_type(opts)
 
-  def for({:list, opts}), do: XList.type(opts)
-  def for({:array, opts}), do: XList.type(opts)
   def for({:set, opts}), do: XSet.type(opts)
 
   def for(ecto_type) do
